cea5c552bacea4953b8149c0ccde3587a95ccb53
nnaleck <foxtrot.kamel@gmail.com>
2022-01-06 12:05:04 +0100

    wip

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from authentication import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
     path('register', views.RegisterView.as_view(), name='auth-register'),
 ]
